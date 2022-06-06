475d99c52137dcd4125291550a9aede9ad25e458
Marinov Vadym <ssadypic@gmail.com>
2022-06-06 15:56:59 +0300

    Готовый калькулятор, без функции очистки по символу

--- Assets/Scripts/Calculating.cs
+++ Assets/Scripts/Calculating.cs
@@ -10,32 +10,57 @@ public class Calculating : MonoBehaviour
     public Text inputDataText;
     ActionEnum act = new ActionEnum();
     
+    // ActionEnum act;
+    
+    
 
     public void ClickButtonToResult(){
-        string a = inputDataText.text;
-        int index=0;
-        string firstNum = "";
-        string secondNum = "";
         
-        char[] aArray = a.ToCharArray();                                                       
+        string a = inputDataText.text;
+        
+        int index=0;
+        
+        
+        char[] aArray = a.ToCharArray();
+        List<char> left = new List<char>();
+        List<char> right = new List<char>();
+
+                                                           
         for(int i = 0; i < aArray.Length; i++){
             if(aArray[i]!= '/' && aArray[i]!= '*' && aArray[i]!= '+' && aArray[i]!= '-'){
-               firstNum += aArray[i]; 
+               
+               left.Add(aArray[i]); 
             }
             else{
                 index = i;
                 break;
             }
         }
-        double firstnum = Convert.ToDouble(firstNum);
+
+        for(int i = index+1; i< aArray.Length; i++){
+            right.Add(aArray[i]); 
+        }
+        string leftStr = new string(left.ToArray());
+        double firstnum = double.Parse(leftStr);
+        string rightStr = new string(right.ToArray());
+        double secondnum = double.Parse(rightStr);
+        
+        
+
+       
+
+        
+        
         
         char actionChar = aArray[index];
         
-        for(int i = index+1;i <aArray.Length; i++){
-            secondNum += aArray[i];
-        }
-        double secondnum = Convert.ToDouble(secondNum);
-
+        // for(int i = index+1;i <aArray.Length; i++){
+        //     secondNum += aArray[i].ToString();
+           
+        // }
+        // double secondnum = Convert.ToDouble(secondNum);
+        // Debug.Log(secondNum);
+        // Debug.Log(firstNum);
         if(actionChar == '/'){
             double result = act.Division(firstnum, secondnum);
             resultText.text = result.ToString();
--- Assets/Scripts/InputNumbers.cs
+++ Assets/Scripts/InputNumbers.cs
@@ -6,11 +6,17 @@ using UnityEngine.UI;
 public class InputNumbers : MonoBehaviour
 {
     public Text textOutput;
+    public Text resultText;
     public string buttonText;
     public string outputData;
 
     public void ClickOnButton(){
         textOutput.text += buttonText;
+    }
+
+    public void RemoveSymbol(){
+        textOutput.text = "";
+        resultText.text = "";
     }
 
 }
