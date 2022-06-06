08d54f7dc7eaa45d39bf0064d40e5bb75ca1df23
Marinov Vadym <ssadypic@gmail.com>
2022-06-06 22:24:02 +0300

    ver. 1.0

--- Assets/Scripts/AlgorythmAction.cs
+++ Assets/Scripts/AlgorythmAction.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AlgorythmAction : MonoBehaviour
+public class AlgorythmAction : MonoBehaviour                            //класс с стратегиями
 {
 
-    public double Division(double firstNum, double secondNum){
+    public double Division(double firstNum, double secondNum){                   //деление
         return firstNum/secondNum;
     }
 
-    public double Sum(double firstNum, double secondNum){
+    public double Sum(double firstNum, double secondNum){                         // сумма
         return firstNum+secondNum;
     }
-    public double Minus(double firstNum, double secondNum){
+    public double Minus(double firstNum, double secondNum){                      //отнимание
         return firstNum-secondNum;
     }
-    public double Multiplication(double firstNum, double secondNum){
+    public double Multiplication(double firstNum, double secondNum){                      //умножение
         return firstNum*secondNum;
     }
 
--- Assets/Scripts/Calculating.cs
+++ Assets/Scripts/Calculating.cs
@@ -5,72 +5,75 @@ using UnityEngine.UI;
 using System;
 
 public class Calculating : MonoBehaviour
-{
-    public Text resultText;
-    public Text inputDataText;
+{ 
+    public Text resultText;                    //результат
+    public Text inputDataText;                   //входящие данные
     AlgorythmAction act = new AlgorythmAction();
-    [HideInInspector] public double firstnum;
-    [HideInInspector] public double secondnum;
-
-    
-    // ActionEnum act;
-    
     
 
     public void ClickButtonToResult(){  
-        string a = inputDataText.text;               //разбитие строки на символы
-        char[] aArray = a.ToCharArray();             //в массив
-        List<char> left = new List<char>();        //списки для будущих чисел //до знака
-        List<char> right = new List<char>();                                  //после знака
+                      
+        char[] aArray = inputDataText.text.ToCharArray();            //разбитие строки на символыв массив
+        List<char> left = new List<char>();                          //списки для будущих чисел //до знака
+        List<char> right = new List<char>();                                                    //после знака
         int index=0;                    //индекс знака в массиве символов
-        if(a == ""){                                   // ошибка при отстутствии даннных
+        if(inputDataText.text == "" || inputDataText.text.Contains(",,")){                                   // ошибка при отстутствии даннных
             resultText.text = "Error";
             return;
-
         }
         
         for(int i = 0; i < aArray.Length; i++){                                                //цикл идет до знака
             if(aArray[i]!= '/' && aArray[i]!= '*' && aArray[i]!= '+' && aArray[i]!= '-'){      //записывая символы в             
                left.Add(aArray[i]);                                                            //массив left
-            }                                                                                  //остановка на знаке
-            else{                                                                              //и запись его индекса
-                index = i;                                                               //в ранее созданую переменную
+            }else{                                                                              //и запись его индекса
+                index = i;                                                                      //в ранее созданую переменную
                 break;
-            }
+            }                                                                                  //остановка на знаке
         }
 
         for(int i = index+1; i< aArray.Length; i++){         //второе число(после знака)
             right.Add(aArray[i]); 
         }
 
-        string leftStr = new string(left.ToArray());               //перевод массива в строку
-        double firstnum = double.Parse(leftStr);                   //из массива в число типа double
+        string leftStr = new string(left.ToArray());               //перевод массива в строку                          
         string rightStr = new string(right.ToArray());
-        double secondnum = double.Parse(rightStr);
 
-        char actionChar = aArray[index];                        //знак действия
-        
-        
-        
+        if(leftStr == "" || leftStr.Contains(",,") || leftStr.StartsWith(",") || rightStr == "" || rightStr.StartsWith(",")||rightStr.Contains(",,")){
+            resultText.text = "Error";
+            return;
+        }
+
+        double secondnum = double.Parse(rightStr);          //из строки в число типа double
+        double firstnum = double.Parse(leftStr);
+        char actionChar = aArray[index];                     //знак действия
+        resultText.text = DoAction(actionChar, firstnum, secondnum);   
+    }
+    
+    public string DoAction(char actionChar, double firstnum, double secondnum){            //метод выполняющий действия с двумя числами
+        double result = 0;                                                                 // в зависимости от знака
         if(actionChar == '/'){
-            double result = act.Division(firstnum, secondnum);
-            resultText.text = result.ToString();
+            result = act.Division(firstnum, secondnum);
+            
         }
 
         if(actionChar == '*'){
-            double result = act.Multiplication(firstnum, secondnum);
-            resultText.text = result.ToString();
+            result = act.Multiplication(firstnum, secondnum);
+            
         }
 
         if(actionChar == '+'){
-            double result = act.Sum(firstnum, secondnum);
-            resultText.text = result.ToString();
+            result = act.Sum(firstnum, secondnum);
+            
         }
 
         if(actionChar == '-'){
-            double result = act.Minus(firstnum, secondnum);
-            resultText.text = result.ToString();
+            result = act.Minus(firstnum, secondnum);
+            
         }
+        string str = result.ToString();
+        return str;
+
+
     }
 
     
